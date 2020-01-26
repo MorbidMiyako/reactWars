3c823cba8cba0b44d764f7465318d08934c4c391
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-01-27 00:08:25 +0100

    your original site fully optimised as far as i could see

--- src/components/CharContainer.js
+++ src/components/CharContainer.js
@@ -5,88 +5,64 @@ import { Button } from "reactstrap";
 
 export default function CharContain() {
   const [chars, setChars] = useState([]);
-  const [next, setNext] = useState();
-  const [previous, setPrevious] = useState();
   const [url, setUrl] = useState(`https://swapi.co/api/people`);
   const [loading, setLoading] = useState(true);
-
-  // const page = (state, setState) => {
-  //   axios
-  //     .get(state)
-  //     .then(res => (setUrl(state), setState(res.data.state)))
-  //     .catch(err => console.log(err));
-  // };
-
-  const nextPage = () => {
-    setLoading(true);
-    axios
-      .get(next)
-      .then(res => (setUrl(next), setNext(res.data.next)))
-      // .then(() => setLoading(false))
-      .catch(err => console.log(err));
-  };
-
-  const prevPage = () => {
-    setLoading(true);
-    axios
-      .get(previous)
-      .then(res => (setUrl(previous), setPrevious(res.data.previous)))
-
-      .catch(err => console.log(err));
-  };
 
   useEffect(() => {
     axios
       .get(url)
       .then(
-        res => (
-          setChars(res.data.results.map(item => item)),
-          setNext(res.data.next),
-          setPrevious(res.data.previous),
+        res => {
+          setChars(res.data)
           setLoading(false)
-        )
+        }
       )
       .catch(err => console.log(err));
   }, [url]);
 
   console.log(chars);
-  console.log("I'm res", next);
   return (
     <>
       <div className="container">
         {loading ? (
           <Loading />
         ) : (
-          <div>
-            <Button
-              id="butt"
-              size="sm"
-              className={previous ? "active" : "notActive"}
-              disabled={!previous}
-              onClick={prevPage}
-            >
-              previous
-            </Button>
+            <div>
+              <Button
+                id="butt"
+                size="sm"
+                className={chars.previous ? "active" : "notActive"}
+                disabled={!chars.previous}
+                onClick={() => {
+                  setUrl(chars.previous)
+                  setLoading(true)
+                }}
+              >
+                previous
+              </Button>
 
-            <Button
-              id="butt"
-              size="sm"
-              disabled={!next}
-              className={next ? "active" : "notActive"}
-              onClick={nextPage}
-            >
-              next
-            </Button>
+              <Button
+                id="butt"
+                size="sm"
+                disabled={!chars.next}
+                className={chars.next ? "active" : "notActive"}
+                onClick={() => {
+                  setUrl(chars.next)
+                  setLoading(true)
+                }}
+              >
+                next
+              </Button>
 
-            {chars.map(char => (
-              <div className="card">
-                <h1>Name:{char.name}</h1>
-                <p>height:{char.height}</p>
-                <p>mass:{char.mass}</p>
-              </div>
-            ))}
-          </div>
-        )}
+              {chars.results.map(char => (
+                <div className="card">
+                  <h1>Name:{char.name}</h1>
+                  <p>height:{char.height}</p>
+                  <p>mass:{char.mass}</p>
+                </div>
+              ))}
+            </div>
+          )}
       </div>
     </>
   );
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,60 +1,23 @@
 import React from "react";
 import { Spinner } from "reactstrap";
 
+const createSpinnerArray = () => {
+  let spinnerArray = []
+  for (let i = 0; i < 54; i++) {
+    spinnerArray.push(1)
+  }
+  return spinnerArray
+}
+
 const Loading = props => {
   return (
     <div className="loader">
-      <Spinner type="grow" className="spin" />
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />{" "}
-      <Spinner type="grow" className="spin" />
+      {createSpinnerArray().map(i => (
+
+        <Spinner type="grow" className="spin" />
+      ))
+      }
+
     </div>
   );
 };
